33c58b2dc9c60ca366d28c66e4eee3f36943cc58
pawel.lyskawinski <lyskawinski@protonmail.com>
2018-11-18 11:33:06 +0100

    fall back to something saner (math library migration will take some time..)

--- math.hh
+++ math.hh
@@ -2,180 +2,160 @@
 
 #include <SDL2/SDL_stdinc.h>
 
-template <uint32_t DIM> struct Vec
+template <uint32_t N> struct Array
 {
-  Vec<DIM>() = default;
-
   float&       operator[](uint32_t i) { return data[i]; }
   const float& operator[](uint32_t i) const { return data[i]; }
 
-  Vec<DIM> operator+(const Vec<DIM>& rhs) const
+  Array<N> clone() const { return *this; }
+
+  void scale(float scale)
   {
-    Vec<DIM> r;
-    for (uint32_t i = 0; i < DIM; ++i)
-      r[i] = data[i] + rhs[i];
+    for (uint32_t i = 0; i < N; ++i)
+      data[i] *= scale;
+  }
+
+  void subtract(const Array<N>& other)
+  {
+    for (uint32_t i = 0; i < N; ++i)
+      data[i] -= other[i];
+  }
+
+  void clear()
+  {
+    for (uint32_t i = 0; i < N; ++i)
+      data[i] = 0.0f;
+  }
+
+  Array<N> operator*(float scale) const
+  {
+    Array<N> r = clone();
+    r.scale(scale);
     return r;
   }
 
-  Vec<DIM> operator-(const Vec<DIM>& rhs) const
+  Array<N> operator-(const Array<N>& other) const
   {
-    Vec<DIM> r;
-    for (uint32_t i = 0; i < DIM; ++i)
-      r[i] = data[i] - rhs[i];
+    Array<N> r = clone();
+    r.subtract(other);
     return r;
   }
 
-  Vec<DIM>& scale(float s)
-  {
-    for (uint32_t i = 0; i < DIM; ++i)
-      data[i] *= s;
-    return *this;
-  }
-
-  Vec<DIM> mul_cross(const Vec<DIM>& rhs)
-  {
-    // @todo: make it work for anything other than DIM=3
-    Vec<DIM> r;
-    r[0] = data[1] * rhs[2] - data[2] * rhs[1];
-    r[1] = data[2] * rhs[0] - data[0] * rhs[2];
-    r[2] = data[0] * rhs[1] - data[1] * rhs[0];
-    return r;
-  }
-
-  float mul_inner(const Vec<DIM>& rhs)
-  {
-    float p = 0.0f;
-    for (uint32_t i = 0; i < DIM; ++i)
-      p += data[i] * rhs[i];
-    return p;
-  }
-
-  Vec<DIM> lerp(const Vec<DIM>& b, float t)
-  {
-    Vec<DIM> r;
-    for (int i = 0; i < DIM; ++i)
-    {
-      float distance = b[i] - data[i];
-      float progress = distance * t;
-      r[i]           = data[i] + progress;
-    }
-    return r;
-  }
-
-  float data[DIM] = {};
+  float data[N];
 };
 
-using Vec4 = Vec<4>;
-using Vec3 = Vec<3>;
-using Vec2 = Vec<2>;
+using Vec2 = Array<2>;
+using Vec3 = Array<3>;
+using Vec4 = Array<4>;
 
-struct Quaternion
+struct Quat : public Vec4
 {
-  Quaternion() = default;
-
-  Quaternion& rotate(Vec3 axis, float angle)
+  explicit Quat(const Vec3& rhs)
+      : Vec4{}
   {
-    axis.scale(SDL_sinf(angle / 2));
-    a = axis[0];
-    b = axis[1];
-    c = axis[2];
-    d = SDL_cosf(angle / 2);
-    return *this;
+    for (uint32_t i = 0; i < 3; ++i)
+      data[i] = rhs[i];
   }
 
-  Vec3 as_vec3() const { return {a, b, c}; }
-
-  Quaternion operator*(const Quaternion& rhs)
+  static Quat rotate(const float angle_rad, const Vec3& axis)
   {
-    Vec3 r = as_vec3().mul_cross(rhs.as_vec3()) + as_vec3().scale(rhs.d) + rhs.as_vec3().scale(d);
-    return {r[0], r[1], r[2], (d * rhs.d) - as_vec3().mul_inner(rhs.as_vec3())};
+    Quat v = Quat(axis * SDL_sinf(angle_rad / 2.0f));
+    v[3]   = SDL_cosf(angle_rad / 2.0f);
+    return v;
   }
-
-  Quaternion& rotate_x(float angle) { return rotate({1.0f, 0.0f, 0.0f}, angle); }
-  Quaternion& rotate_y(float angle) { return rotate({0.0f, 1.0f, 0.0f}, angle); }
-  Quaternion& rotate_z(float angle) { return rotate({0.0f, 0.0f, 1.0f}, angle); }
-
-  float a = 0.0f;
-  float b = 0.0f;
-  float c = 0.0f;
-  float d = 1.0f;
 };
 
 struct Mat4
 {
-  Mat4() = default;
-
-  explicit Mat4(const Quaternion& q)
+  Mat4()
+      : columns{}
   {
-    float a = q.d;
-    float b = q.a;
-    float c = q.b;
-    float d = q.c;
+  }
 
+  explicit Mat4(const Quat& quat)
+      : columns{}
+  {
+    float a  = quat[3];
+    float b  = quat[0];
+    float c  = quat[1];
+    float d  = quat[2];
     float a2 = a * a;
     float b2 = b * b;
     float c2 = c * c;
     float d2 = d * d;
 
-    M[0][0] = a2 + b2 - c2 - d2;
-    M[0][1] = 2.0f * (b * c + a * d);
-    M[0][2] = 2.0f * (b * d - a * c);
-    M[0][3] = 0.0f;
+    columns[0][0] = a2 + b2 - c2 - d2;
+    columns[0][1] = 2.f * (b * c + a * d);
+    columns[0][2] = 2.f * (b * d - a * c);
+    columns[0][3] = 0.f;
 
-    M[1][0] = 2 * (b * c - a * d);
-    M[1][1] = a2 - b2 + c2 - d2;
-    M[1][2] = 2.0f * (c * d + a * b);
-    M[1][3] = 0.0f;
+    columns[1][0] = 2 * (b * c - a * d);
+    columns[1][1] = a2 - b2 + c2 - d2;
+    columns[1][2] = 2.f * (c * d + a * b);
+    columns[1][3] = 0.f;
 
-    M[2][0] = 2.0f * (b * d + a * c);
-    M[2][1] = 2.0f * (c * d - a * b);
-    M[2][2] = a2 - b2 - c2 + d2;
-    M[2][3] = 0.0f;
+    columns[2][0] = 2.f * (b * d + a * c);
+    columns[2][1] = 2.f * (c * d - a * b);
+    columns[2][2] = a2 - b2 - c2 + d2;
+    columns[2][3] = 0.f;
 
-    M[3][0] = 0.0f;
-    M[3][1] = 0.0f;
-    M[3][2] = 0.0f;
-    M[3][3] = 1.0f;
+    columns[3][0] = 0.f;
+    columns[3][1] = 0.f;
+    columns[3][2] = 0.f;
+    columns[3][3] = 1.f;
   }
 
-  Mat4& translate(const Vec3& in)
+  void identity()
   {
-    identity();
-    M[3][0] = in[0];
-    M[3][1] = in[1];
-    M[3][2] = in[2];
-    return *this;
+    for (auto& column : columns)
+      column.clear();
+
+    for (uint32_t row = 0; row < 4; ++row)
+      for (uint32_t col = 0; col < 4; ++col)
+        if (row == col)
+          columns[row][col] = 1.0f;
   }
 
-  Mat4& identity()
+  Vec4&       operator[](uint32_t i) { return columns[i]; }
+  const Vec4& operator[](uint32_t i) const { return columns[i]; }
+
+  static Mat4 Translation(float x, float y, float z)
   {
-    for (uint32_t i = 0; i < 4; ++i)
-      for (uint32_t j = 0; j < 4; ++j)
-        M[i][j] = (i == j) ? 1.f : 0.f;
-    return *this;
+    Mat4 r;
+    r.identity();
+    r[3][0] = x;
+    r[3][1] = y;
+    r[3][2] = z;
+    return r;
   }
 
-  Mat4& scale(float x, float y, float z)
+  static Mat4 Scale(float x, float y, float z)
   {
-    M[0].scale(x);
-    M[1].scale(y);
-    M[2].scale(z);
-    return *this;
+    Mat4 r;
+    r.identity();
+    r.columns[0].scale(x);
+    r.columns[1].scale(y);
+    r.columns[2].scale(z);
+    return r;
   }
 
-  Mat4& scale(float val) { return scale(val, val, val); }
+  float* data() { return columns[0].data; }
 
-  Mat4 operator*(const Mat4& rhs)
+  Mat4 operator*(const Mat4 rhs)
   {
     Mat4 result;
+
     for (uint32_t c = 0; c < 4; ++c)
       for (uint32_t r = 0; r < 4; ++r)
+      {
+        result.columns[c][r] = 0.f;
         for (uint32_t k = 0; k < 4; ++k)
-          result.M[c][r] += M[k][r] * rhs.M[c][k];
+          result.columns[c][r] += columns[k][r] * rhs.columns[c][k];
+      }
+
     return result;
   }
 
-  float* data() { return M[0].data; }
-
-  Vec4 M[4] = {};
+  Vec4 columns[4];
 };
+
--- update_jobs.cc
+++ update_jobs.cc
@@ -163,31 +163,56 @@ void animate_entity(SimpleEntity& entity, Ecs& ecs, SceneGraph& scene_graph, flo
   }
 }
 
+void multiply(quat result, quat lhs, quat rhs) { quat_mul(result, lhs, rhs); }
+
+template <typename... Args> void multiply(quat result, quat lhs, quat rhs, Args... args)
+{
+  quat tmp = {};
+  quat_mul(tmp, lhs, rhs);
+  multiply(result, tmp, args...);
+}
+
+void multiply(mat4x4 result, mat4x4 lhs, mat4x4 rhs) { mat4x4_mul(result, lhs, rhs); }
+
+template <typename... Args> void multiply(mat4x4 result, mat4x4 lhs, mat4x4 rhs, Args... args)
+{
+  mat4x4 tmp = {};
+  mat4x4_mul(tmp, lhs, rhs);
+  multiply(result, tmp, args...);
+}
+
 } // namespace
 
 namespace update {
 
 void helmet_job(ThreadJobData tjd)
 {
-  Mat4 world_transform = Mat4().translate({tjd.game.vr_level_goal[0], 3.0f, tjd.game.vr_level_goal[1]}) *
-                         Mat4(Quaternion().rotate_x(to_rad(180.0))) * Mat4().identity().scale(2.0f);
+  Mat4 world_transform = Mat4::Translation(tjd.game.vr_level_goal[0], 3.0f, tjd.game.vr_level_goal[1]) *
+                         Mat4(Quat::rotate(to_rad(180.0), Vec3{1.0f, 0.0f, 0.0f})) * Mat4::Scale(1.6f, 1.6f, 1.6f);
 
-  mat4x4* tmp = reinterpret_cast<mat4x4*>(world_transform.data());
-  tjd.game.helmet_entity.recalculate_node_transforms(tjd.game.ecs, tjd.game.helmet, *tmp);
+  mat4x4* ptr = reinterpret_cast<mat4x4*>(world_transform.data());
+  tjd.game.helmet_entity.recalculate_node_transforms(tjd.game.ecs, tjd.game.helmet, *ptr);
 }
 
 void robot_job(ThreadJobData tjd)
 {
-  //////////////////////////////////////////////////////////////////////////////
-  // Putting robot in initial standing position and fixing
-  // the orientation to match camera angle
-  //////////////////////////////////////////////////////////////////////////////
+  vec3 x_axis = {1.0, 0.0, 0.0};
+  vec3 y_axis = {0.0, 1.0, 0.0};
+  vec3 z_axis = {0.0, 0.0, 1.0};
 
-  float back_rotation = to_rad(0.0f);
-  if (tjd.game.player_position[0] < tjd.game.cameras.gameplay.position[0])
-    back_rotation = to_rad(180.0f);
+  quat standing_pose = {};
+  quat_rotate(standing_pose, to_rad(180.0), x_axis);
+
+  quat rotate_back = {};
+  quat_rotate(rotate_back,
+              tjd.game.player_position[0] < tjd.game.cameras.gameplay.position[0] ? to_rad(180.0f) : to_rad(0.0f),
+              y_axis);
+
   float x_delta = tjd.game.player_position[0] - tjd.game.cameras.gameplay.position[0];
   float z_delta = tjd.game.player_position[2] - tjd.game.cameras.gameplay.position[2];
+
+  quat camera = {};
+  quat_rotate(camera, static_cast<float>(SDL_atan(z_delta / x_delta)), y_axis);
 
   //////////////////////////////////////////////////////////////////////////////
   // Adaptive model tilt when moving
@@ -200,10 +225,19 @@ void robot_job(ThreadJobData tjd)
   vec2  corrected_velocity_vector = {velocity_length * SDL_cosf(relative_velocity_angle),
                                     velocity_length * SDL_sinf(relative_velocity_angle)};
 
-  Quaternion orientation = Quaternion().rotate_x(to_rad(180.0)) * Quaternion().rotate_y(back_rotation) *
-                           Quaternion().rotate_y(SDL_atanf(z_delta / x_delta)) *
-                           Quaternion().rotate_x(8.0f * corrected_velocity_vector[0]) *
-                           Quaternion().rotate_z(-8.0f * corrected_velocity_vector[1]);
+  quat movement_tilt[2] = {};
+  quat_rotate(movement_tilt[0], 8.0f * corrected_velocity_vector[0], x_axis);
+  quat_rotate(movement_tilt[1], -8.0f * corrected_velocity_vector[1], z_axis);
+
+  quat orientation = {};
+  multiply(orientation, standing_pose, rotate_back, camera, movement_tilt[0], movement_tilt[1]);
+
+  mat4x4 translation_matrix = {};
+  mat4x4_translate(translation_matrix, tjd.game.player_position[0], tjd.game.player_position[1] - 1.0f,
+                   tjd.game.player_position[2]);
+
+  mat4x4 rotation_matrix = {};
+  mat4x4_from_quat(rotation_matrix, orientation);
 
   Mat4 world_transform =
       Mat4().translate({tjd.game.player_position[0], tjd.game.player_position[1] - 1.0f, tjd.game.player_position[2]}) *
