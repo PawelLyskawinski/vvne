cf221cf0cff79438f4579602477cfdabbc4c8b7f
plyskawinski <pawel.lyskawinski@techland.pl>
2019-07-16 13:39:37 +0200

    optimized framebuffers creation

--- sources/engine/engine_framebuffers.cc
+++ sources/engine/engine_framebuffers.cc
@@ -6,18 +6,18 @@ void shadowmap(Engine& engine)
 {
   RenderPass& render_pass = engine.render_passes.shadowmap;
 
+  VkFramebufferCreateInfo ci = {
+      .sType           = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+      .renderPass      = render_pass.render_pass,
+      .attachmentCount = 1,
+      .width           = SHADOWMAP_IMAGE_DIM,
+      .height          = SHADOWMAP_IMAGE_DIM,
+      .layers          = 1,
+  };
+
   for (unsigned i = 0; i < render_pass.framebuffers_count; ++i)
   {
-    VkFramebufferCreateInfo ci = {
-        .sType           = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
-        .renderPass      = render_pass.render_pass,
-        .attachmentCount = 1,
-        .pAttachments    = &engine.shadowmap_cascade_image_views[i],
-        .width           = SHADOWMAP_IMAGE_DIM,
-        .height          = SHADOWMAP_IMAGE_DIM,
-        .layers          = 1,
-    };
-
+    ci.pAttachments = &engine.shadowmap_cascade_image_views[i];
     vkCreateFramebuffer(engine.device, &ci, nullptr, &render_pass.framebuffers[i]);
   }
 }
@@ -26,30 +26,21 @@ void skybox(Engine& engine)
 {
   RenderPass& render_pass = engine.render_passes.skybox;
 
+  VkImageView attachments[] = {VK_NULL_HANDLE, engine.msaa_color_image.image_view};
+
+  VkFramebufferCreateInfo ci = {
+      .sType           = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+      .renderPass      = render_pass.render_pass,
+      .attachmentCount = (VK_SAMPLE_COUNT_1_BIT == engine.MSAA_SAMPLE_COUNT) ? 2u : 1u,
+      .pAttachments    = attachments,
+      .width           = engine.extent2D.width,
+      .height          = engine.extent2D.height,
+      .layers          = 1,
+  };
+
   for (uint32_t i = 0; i < render_pass.framebuffers_count; ++i)
   {
-    VkImageView attachments_msaa[]    = {engine.swapchain_image_views[i], engine.msaa_color_image.image_view};
-    VkImageView attachments_no_msaa[] = {engine.swapchain_image_views[i]};
-
-    VkFramebufferCreateInfo ci = {
-        .sType      = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
-        .renderPass = render_pass.render_pass,
-        .width      = engine.extent2D.width,
-        .height     = engine.extent2D.height,
-        .layers     = 1,
-    };
-
-    if (VK_SAMPLE_COUNT_1_BIT == engine.MSAA_SAMPLE_COUNT)
-    {
-      ci.attachmentCount = SDL_arraysize(attachments_no_msaa);
-      ci.pAttachments    = attachments_no_msaa;
-    }
-    else
-    {
-      ci.attachmentCount = SDL_arraysize(attachments_msaa);
-      ci.pAttachments    = attachments_msaa;
-    }
-
+    attachments[0] = engine.swapchain_image_views[i];
     vkCreateFramebuffer(engine.device, &ci, nullptr, &render_pass.framebuffers[i]);
   }
 }
@@ -58,32 +49,21 @@ void color_and_depth(Engine& engine)
 {
   RenderPass& render_pass = engine.render_passes.color_and_depth;
 
+  VkImageView attachments[] = {VK_NULL_HANDLE, engine.depth_image.image_view, engine.msaa_color_image.image_view};
+
+  VkFramebufferCreateInfo ci = {
+      .sType           = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+      .renderPass      = render_pass.render_pass,
+      .attachmentCount = (VK_SAMPLE_COUNT_1_BIT == engine.MSAA_SAMPLE_COUNT) ? 2u : 3u,
+      .pAttachments    = attachments,
+      .width           = engine.extent2D.width,
+      .height          = engine.extent2D.height,
+      .layers          = 1,
+  };
+
   for (uint32_t i = 0; i < render_pass.framebuffers_count; ++i)
   {
-    VkImageView attachments_msaa[] = {engine.swapchain_image_views[i], engine.depth_image.image_view,
-                                      engine.msaa_color_image.image_view};
-
-    VkImageView attachments_no_msaa[] = {engine.swapchain_image_views[i], engine.depth_image.image_view};
-
-    VkFramebufferCreateInfo ci = {
-        .sType      = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
-        .renderPass = render_pass.render_pass,
-        .width      = engine.extent2D.width,
-        .height     = engine.extent2D.height,
-        .layers     = 1,
-    };
-
-    if (VK_SAMPLE_COUNT_1_BIT == engine.MSAA_SAMPLE_COUNT)
-    {
-      ci.attachmentCount = SDL_arraysize(attachments_no_msaa);
-      ci.pAttachments    = attachments_no_msaa;
-    }
-    else
-    {
-      ci.attachmentCount = SDL_arraysize(attachments_msaa);
-      ci.pAttachments    = attachments_msaa;
-    }
-
+    attachments[0] = engine.swapchain_image_views[i];
     vkCreateFramebuffer(engine.device, &ci, nullptr, &render_pass.framebuffers[i]);
   }
 }
@@ -92,30 +72,21 @@ void gui(Engine& engine)
 {
   RenderPass& render_pass = engine.render_passes.gui;
 
+  VkImageView attachments[] = {VK_NULL_HANDLE, engine.msaa_color_image.image_view};
+
+  VkFramebufferCreateInfo ci = {
+      .sType           = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+      .renderPass      = render_pass.render_pass,
+      .attachmentCount = (VK_SAMPLE_COUNT_1_BIT == engine.MSAA_SAMPLE_COUNT) ? 1u : 2u,
+      .pAttachments    = attachments,
+      .width           = engine.extent2D.width,
+      .height          = engine.extent2D.height,
+      .layers          = 1,
+  };
+
   for (uint32_t i = 0; i < render_pass.framebuffers_count; ++i)
   {
-    VkImageView attachments_msaa[]    = {engine.swapchain_image_views[i], engine.msaa_color_image.image_view};
-    VkImageView attachments_no_msaa[] = {engine.swapchain_image_views[i]};
-
-    VkFramebufferCreateInfo ci = {
-        .sType      = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
-        .renderPass = render_pass.render_pass,
-        .width      = engine.extent2D.width,
-        .height     = engine.extent2D.height,
-        .layers     = 1,
-    };
-
-    if (VK_SAMPLE_COUNT_1_BIT == engine.MSAA_SAMPLE_COUNT)
-    {
-      ci.attachmentCount = SDL_arraysize(attachments_no_msaa);
-      ci.pAttachments    = attachments_no_msaa;
-    }
-    else
-    {
-      ci.attachmentCount = SDL_arraysize(attachments_msaa);
-      ci.pAttachments    = attachments_msaa;
-    }
-
+    attachments[0] = engine.swapchain_image_views[i];
     vkCreateFramebuffer(engine.device, &ci, nullptr, &render_pass.framebuffers[i]);
   }
 }
