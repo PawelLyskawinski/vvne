c429b2fe077bbceb39896014c6924a6369d2da09
plyskawinski <pawel.lyskawinski@techland.pl>
2019-09-24 17:05:15 +0200

    Corrected flipped values in model_adjustment calculation

--- sources/game_generate_sdf_font.cc
+++ sources/game_generate_sdf_font.cc
@@ -1,5 +1,6 @@
 #include "game.hh"
 #include <SDL2/SDL_assert.h>
+#include <SDL2/SDL_log.h>
 #include <algorithm>
 
 static uint32_t find_character_index(const uint8_t* begin, const uint8_t* end, const uint8_t search)
@@ -21,7 +22,8 @@ GenerateSdfFontCommandResult generate_sdf_font(const GenerateSdfFontCommand& cmd
   const Vec2     scaling               = uv_adjusted.scale(cmd.scaling);
   const Vec2     inverted_texture_size = cmd.texture_size.invert();
   const Vec2     model_adjustment =
-      scaling + (char_offsets.scale(cmd.scaling).scale(cmd.texture_size.scale(2.0f))) - Vec2(1.0f + cmd.cursor, 2.0f);
+      scaling + (char_offsets.scale(cmd.scaling).scale(cmd.texture_size.scale(2.0f))) - Vec2(2.0f + cmd.cursor, 1.0f);
+
   const Mat4x4 translation = Mat4x4::Translation(Vec3(model_adjustment, 0.0f) + cmd.position);
   const Mat4x4 scale       = Mat4x4::Scale(Vec3(uv_adjusted.scale(cmd.scaling), 1.0f));
 
