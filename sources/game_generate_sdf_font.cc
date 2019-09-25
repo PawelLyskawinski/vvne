02e9948e20a167e221709752617fd784128fe3f1
pawel.lyskawinski <lyskawinski@protonmail.com>
2019-09-25 08:14:41 +0200

    finally fixed sdf font rendering

--- sources/game_generate_sdf_font.cc
+++ sources/game_generate_sdf_font.cc
@@ -1,6 +1,5 @@
 #include "game.hh"
 #include <SDL2/SDL_assert.h>
-#include <SDL2/SDL_log.h>
 #include <algorithm>
 
 static uint32_t find_character_index(const uint8_t* begin, const uint8_t* end, const uint8_t search)
@@ -10,27 +9,28 @@ static uint32_t find_character_index(const uint8_t* begin, const uint8_t* end, c
   return static_cast<uint32_t>(std::distance(begin, it));
 }
 
+static Vec2 divide(const Vec2& lhs, const Vec2& rhs) { return Vec2(lhs.x / rhs.x, lhs.y / rhs.y); }
+
 GenerateSdfFontCommandResult generate_sdf_font(const GenerateSdfFontCommand& cmd)
 {
-  const uint8_t* begin                 = cmd.lookup_table;
-  const uint8_t* end                   = begin + cmd.characters_pool_count;
-  const SdfChar& char_data             = cmd.character_data[find_character_index(begin, end, cmd.character)];
-  const Vec2     char_size             = Vec2(char_data.width, char_data.height);
-  const Vec2     char_offsets          = Vec2(char_data.xoffset, char_data.yoffset);
-  const Vec2     char_position         = Vec2(char_data.x, char_data.y);
-  const Vec2     uv_adjusted           = char_size.scale(cmd.texture_size).scale(Vec2(0.5f, 0.25f));
-  const Vec2     scaling               = uv_adjusted.scale(cmd.scaling);
-  const Vec2     inverted_texture_size = cmd.texture_size.invert();
+  const uint8_t* begin         = cmd.lookup_table;
+  const uint8_t* end           = begin + cmd.characters_pool_count;
+  const SdfChar& char_data     = cmd.character_data[find_character_index(begin, end, cmd.character)];
+  const Vec2     char_size     = Vec2(char_data.width, char_data.height);
+  const Vec2     char_offsets  = Vec2(char_data.xoffset, char_data.yoffset);
+  const Vec2     char_position = Vec2(char_data.x, char_data.y);
+  const Vec2     uv_adjusted   = divide(char_size, cmd.texture_size).scale(Vec2(0.5f, 0.25f));
+  const Vec2     scaling       = uv_adjusted.scale(cmd.scaling);
 
   const Vec2 model_adjustment =
-      scaling + (char_offsets.scale(cmd.scaling).scale(cmd.texture_size.scale(2.0f))) - Vec2(2.0f + cmd.cursor, 1.0f);
+      scaling + divide(char_offsets, cmd.texture_size).scale(0.5f * cmd.scaling) - Vec2(2.0f - cmd.cursor, 1.0f);
 
   const Mat4x4 translation = Mat4x4::Translation(Vec3(model_adjustment, 0.0f) + cmd.position);
   const Mat4x4 scale       = Mat4x4::Scale(Vec3(uv_adjusted.scale(cmd.scaling), 1.0f));
 
   GenerateSdfFontCommandResult result = {
-      .character_coordinate = char_position.scale(inverted_texture_size),
-      .character_size       = char_size.scale(inverted_texture_size),
+      .character_coordinate = divide(char_position, cmd.texture_size),
+      .character_size       = divide(char_size, cmd.texture_size),
       .transform            = translation * scale,
       .cursor_movement      = cmd.scaling * (static_cast<float>(char_data.xadvance) / cmd.texture_size.x),
   };
