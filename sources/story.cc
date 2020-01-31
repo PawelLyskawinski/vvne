df4a80c7f99e6ed0d7d2f07a12c46573702e54e7
pawel.lyskawinski <lyskawinski@protonmail.com>
2020-01-31 09:18:27 +0100

    Partial implementation of story update

--- sources/story.cc
+++ sources/story.cc
@@ -1,6 +1,7 @@
 #include "engine/allocators.hh"
 #include "engine/hierarchical_allocator.hh"
 #include "story_editor.hh"
+#include <algorithm>
 
 namespace story {
 
@@ -27,23 +28,71 @@ uint32_t gather_active_entities(const State states[], uint32_t count, uint32_t* 
   return accumulate_indices(states, states + count, dst_indicies, is_active);
 }
 
+//
+// Returns:
+// true  - node still active
+// false - node finished executing
+//
+bool update(Data& data, uint32_t entity_idx)
+{
+  switch (data.nodes[entity_idx])
+  {
+  case Node::Start:
+    data.node_states[entity_idx] = State::Finished;
+    return false;
+  default:
+    return true;
+  }
+}
+
 } // namespace
 
 void tick(HierarchicalAllocator& allocator, Data& data)
 {
+  uint32_t  active_entites_capacity = 256;
+  uint32_t* active_entities         = allocator.allocate_threadsafe<uint32_t>(active_entites_capacity);
+  uint32_t  active_entities_count   = gather_active_entities(data.node_states, data.entity_count, active_entities);
+
   //
-  // 1. gather all "active" nodes.
-  // 2. for each
-  //    2.1 check if they can progress
-  //        if yes -> make the next nodes active and add them to the list
+  // partition active entities into those which are still active and already finished
+  //
+  // [ A A A A A ] --> [ A A F F F ]
+  //                         *
+  //                         partition point
   //
 
-  uint32_t                  active_entites_capacity = 256;
-  uint32_t*                 active_entities         = allocator.allocate_threadsafe<uint32_t>(active_entites_capacity);
-  [[maybe_unused]] uint32_t active_entities_count =
-      gather_active_entities(data.node_states, data.entity_count, active_entities);
+  auto      call_update     = [&data](uint32_t entity_idx) { return update(data, entity_idx); };
+  uint32_t* partition_point = std::partition(active_entities, active_entities + active_entities_count, call_update);
+  uint32_t  finished_count  = std::distance(partition_point, active_entities + active_entities_count);
 
-  // do the work
+  while (finished_count)
+  {
+    //
+    // [ A A F F F ] --> [ A A F F F A_new A_new A_new ]
+    //                               *
+    //                               new_active
+
+    uint32_t* new_active              = active_entities + active_entities_count;
+    uint32_t* new_active_accummulator = new_active;
+    for (uint32_t connection_idx = 0; connection_idx < data.connections_count; ++connection_idx)
+    {
+      const Connection& c         = data.connections[connection_idx];
+      auto              is_source = [c](uint32_t entity_idx) { return c.src_node_idx == entity_idx; };
+      if (std::any_of(partition_point, partition_point + finished_count, is_source))
+      {
+        *new_active_accummulator++ = c.dst_node_idx;
+      }
+    }
+
+    if(new_active != new_active_accummulator)
+    {
+        // @todo
+    }
+    else
+    {
+        finished_count = 0;
+    }
+  }
 
   allocator.free_threadsafe(active_entities, active_entites_capacity);
 }
