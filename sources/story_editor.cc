c73816f7fb00a9539430593928b1b99226d52fa9
pawel.lyskawinski <lyskawinski@protonmail.com>
2020-02-05 09:12:50 +0100

    Removed fileops duplication

--- sources/story_editor.cc
+++ sources/story_editor.cc
@@ -1,5 +1,6 @@
 #include "story_editor.hh"
 #include "color_palette.hh"
+#include "engine/fileops.hh"
 #include "imgui.h"
 #include <SDL2/SDL_log.h>
 #include <algorithm>
@@ -99,36 +100,6 @@ Vec2 get_mouse_state()
 }
 
 } // namespace
-
-struct FileOps
-{
-  explicit FileOps(SDL_RWops* handle)
-      : handle(handle)
-  {
-  }
-
-  template <typename T> void serialize(const T& data)
-  {
-    SDL_RWwrite(handle, &data, sizeof(T), 1);
-  }
-
-  template <typename T> void serialize(const T* data, uint32_t count)
-  {
-    SDL_RWwrite(handle, data, sizeof(T), count);
-  }
-
-  template <typename T> void deserialize(T& data)
-  {
-    SDL_RWread(handle, &data, sizeof(T), 1);
-  }
-
-  template <typename T> void deserialize(T* data, uint32_t count)
-  {
-    SDL_RWread(handle, data, sizeof(T), count);
-  }
-
-  SDL_RWops* handle;
-};
 
 struct ScaledBox
 {
