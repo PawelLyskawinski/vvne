8b4d9268f6a44bc42ae7a89c9fbafa19ec84ce7f
pawel.lyskawinski <lyskawinski@protonmail.com>
2020-02-15 20:13:00 +0100

    Removed unnecessary parameter

--- sources/gui_lines_renderer.hh
+++ sources/gui_lines_renderer.hh
@@ -18,7 +18,6 @@ struct GuiLinesUpdate
   float      camera_y_pitch_radians   = 0.0f;
   float      player_speed             = 0.0f;
   Vec2       debug                    = {};
-  VkExtent2D screen_extent2D          = {};
 
   void operator()(LinesRenderer& renderer) const;
 };
