a7061de179a0b76bde17eb03f976bb775570b04e
pawel.lyskawinski <lyskawinski@protonmail.com>
2020-03-07 09:52:23 +0100

    Refactored block allocator visualizer

--- sources/engine/block_allocator_visualizer.cc
+++ sources/engine/block_allocator_visualizer.cc
@@ -1,43 +1,57 @@
 #include "block_allocator_visualizer.hh"
 #include <imgui.h>
 
-void block_allocator_visualize(const BlockAllocator& allocator)
+namespace {
+
+class Renderer
 {
-  const float    max_width = ImGui::GetWindowWidth() * 0.98f;
-  const uint32_t max_size  = allocator.get_max_size();
-  uint32_t       counter   = 0;
-  char           name_buffer[128];
-
-  enum class BlockType
+public:
+  explicit Renderer(bool start_state)
+      : state_fip_flop(start_state)
   {
-    FreeSpace,
-    UsedSpace
-  };
+  }
 
-  auto draw_button = [&name_buffer, &counter, max_size, max_width](BlockType type, uint64_t size) {
-    if (counter)
+  void draw(const float length, const char* name)
+  {
+    if (first_element_rendered)
     {
       ImGui::SameLine(0.0f, 0.0f);
     }
-    const float length = max_width * (static_cast<float>(size) / static_cast<float>(max_size));
-    // TODO This function can be used only once thanks to naming collisions.. fix it!
-    SDL_snprintf(name_buffer, 128, "%s##%u", "block_allocator_visualize", counter++);
-    ImGui::ColorButton(name_buffer,
-                       (BlockType::FreeSpace == type) ? ImVec4(0.1f, 0.1f, 0.1f, 0.0f) : ImVec4(1.0f, 0.0f, 0.0f, 0.1f),
+    else
+    {
+      first_element_rendered = true;
+    }
+
+    ImGui::ColorButton(name, state_fip_flop ? ImVec4(1.0f, 0.0f, 0.0f, 0.1f) : ImVec4(0.1f, 0.1f, 0.1f, 0.0f),
                        ImGuiColorEditFlags_NoTooltip, ImVec2(length, 20));
-  };
 
-  // @TODO for each adjacent used/free strides draw single colored button
+    state_fip_flop = !state_fip_flop;
+  }
 
-  bool           state          = allocator.is_block_used(0);
-  const uint32_t block_capacity = allocator.get_block_capacity();
-  uint64_t       idx            = 0;
+private:
+  bool first_element_rendered = false;
+  bool state_fip_flop         = false;
+};
 
-  while (block_capacity > idx)
+} // namespace
+
+void block_allocator_visualize(const BlockAllocator& allocator)
+{
+  const float    max_width        = ImGui::GetWindowWidth() * 0.98f;
+  const uint32_t max_size         = allocator.get_max_size();
+  const uint32_t block_capacity   = allocator.get_block_capacity();
+  char           name_buffer[128] = {};
+  uint64_t       adjacent_count   = 0;
+
+  Renderer renderer(allocator.is_block_used(0));
+  for (uint32_t idx = 0; block_capacity > idx; idx += adjacent_count)
   {
-    uint64_t adjacent_count = allocator.calc_adjacent_blocks_count(idx);
-    draw_button(state ? BlockType::UsedSpace : BlockType::FreeSpace, allocator.get_block_size() * adjacent_count);
-    idx += adjacent_count;
-    state = !state;
+    SDL_snprintf(name_buffer, 128, "%s##%u", "block_allocator_visualize", idx);
+
+    adjacent_count                    = allocator.calc_adjacent_blocks_count(idx);
+    const float neighbour_blocks_size = static_cast<float>(allocator.get_block_size() * adjacent_count);
+    const float length                = max_width * neighbour_blocks_size / static_cast<float>(max_size);
+
+    renderer.draw(length, name_buffer);
   }
 }
