db9d5a4a8edf9407b2ddce9999d01083f02596e0
pawel.lyskawinski <lyskawinski@protonmail.com>
2020-10-15 10:01:04 +0200

    removed unused threadsafe allocator api

--- sources/engine/hierarchical_allocator.cc
+++ sources/engine/hierarchical_allocator.cc
@@ -1,31 +1,8 @@
 #include "hierarchical_allocator.hh"
 #include "allocators.hh"
 
-namespace {
-
-class ScopedLock
-{
-public:
-  explicit ScopedLock(SDL_mutex* lock)
-      : lock(lock)
-  {
-    SDL_LockMutex(lock);
-  }
-
-  ~ScopedLock()
-  {
-    SDL_UnlockMutex(lock);
-  }
-
-private:
-  SDL_mutex* lock;
-};
-
-} // namespace
-
 void HierarchicalAllocator::init()
 {
-  access_mutex = SDL_CreateMutex();
   free_list_5MB.init(5_MB);
   block_allocator_1kb.init(1_KB, 512);
   block_allocator_10kb.init(10_KB, 512);
@@ -33,7 +10,6 @@ void HierarchicalAllocator::init()
 
 void HierarchicalAllocator::teardown()
 {
-  SDL_DestroyMutex(access_mutex);
   block_allocator_10kb.teardown();
   block_allocator_1kb.teardown();
   free_list_5MB.teardown();
@@ -56,12 +32,6 @@ uint8_t* HierarchicalAllocator::allocate_bytes(unsigned size)
   }
 }
 
-uint8_t* HierarchicalAllocator::allocate_bytes_threadsafe(unsigned size)
-{
-  ScopedLock lock(access_mutex);
-  return allocate_bytes(size);
-}
-
 void HierarchicalAllocator::free_bytes(uint8_t* free_me, unsigned size)
 {
   size = align(size, 16u);
@@ -77,10 +47,4 @@ void HierarchicalAllocator::free_bytes(uint8_t* free_me, unsigned size)
   {
     free_list_5MB.free_bytes(free_me, size);
   }
-}
-
-void HierarchicalAllocator::free_bytes_threadsafe(uint8_t* free_me, unsigned size)
-{
-  ScopedLock lock(access_mutex);
-  free_bytes(free_me, size);
 }
